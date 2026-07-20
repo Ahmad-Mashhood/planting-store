0cb819d635e4bee3945b6c1e3a48cd9a8035c9f4
Ahmad-Mashhood <ahmadmashhood.bcs018@gmail.com>
2026-07-20 07:31:49 +0500

    Support Vercel deployment with dynamic base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/planting-store",
+  base: process.env.VERCEL ? '/' : '/planting-store',
   plugins: [react()],
 })
